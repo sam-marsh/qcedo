659b6cd437416497f85da7acda0dcd025bace1d9
Sam Marsh <sam.t.marsh@gmail.com>
2018-06-04 20:30:45 +0800

    initial version complete, circuit not yet optimised

--- qcedo.py
+++ qcedo.py
@@ -14,22 +14,52 @@ def memoize(f):
 
 # computes walsh function
 @memoize
-def wal(m, n):
+def wal(N, m, n):
     if m == 0:
         return 1
     if m == 1:
-        return 1 if n <= m // 2 - 1 else -1
-    return wal(m//2, 2*n) * wal(m - 2*(m//2), n)
+        return 1 if n <= N // 2 - 1 else -1
+    return wal(N, m//2, (2*n) % N) * wal(N, m - 2*(m//2), n)
 
-# computes Walsh transform coefficients F(m)=sum from 0 to N-1 of f(n) wal(m, n)
+# computes Walsh transform coefficients F(m)=sum from 0 to N-1 of f(n)*wal(m, n)
 def wc(f, m, N):
-    return sp.simplify(sum([f(n)*wal(m, n) for n in range(N)])/N)
+    return sp.simplify(sum([f(n)*wal(N, m, n) for n in range(N)])/N)
 
 # the input function! computes the diagonal entries of the operator which
 # we desire to exponentiate
 def f(n):
-    return -n * sp.Symbol('t')
+    return -(2**n) * sp.Symbol('t')
+
+def gray(x):
+    return (x >> 1) ^ x
+
+def msb(x):
+    i = 0
+    while (x > 1):
+        x >>= 1
+        i += 1
+    return i
+
+def generate_gates(n, fn):
+    N = 2**n
+    for i in range(n):
+        print('\t\tqubit q%d' % (i))
+    print('\n')
+    for i in range(1, N):
+        j = gray(i)
+        z_rot = -2 * wc(fn, j, N)
+        if z_rot != 0:
+            q = msb(j)
+            l = []
+            for k in range(0, q):
+                if (j >> k) & 1 == 1:
+                    l.append(k)
+            for k in reversed(l):
+                print('\tcnot q%d,q%d' % (k, q))
+            print('\trz(%s) q%d' % (z_rot, q))
+            for k in l:
+                print('\tcnot q%d,q%d' % (k, q))
+
 
 if __name__ == '__main__':
-    N = 8
-    print(wc(f, 0, N))
+    generate_gates(3, f)
